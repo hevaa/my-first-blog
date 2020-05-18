2926effcb01929dd8d25f6c8cc30cca1094d959f
Heather <heather122333@live.co.uk>
2020-05-18 19:11:11 +0100

    Unit: we have a homepage

--- blog/tests.py
+++ blog/tests.py
@@ -2,8 +2,8 @@ from django.urls import resolve
 from django.test import TestCase
 from blog.views import home_page  
 
-#class HomePageTest(TestCase):
+class HomePageTest(TestCase):
 
- #   def test_root_url_resolves_to_home_page_view(self):
- #       found = resolve('/')
- #       self.assertEqual(found.func, home_page)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
