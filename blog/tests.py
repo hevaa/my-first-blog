cc3e5fd2a772b598e2e7144db30c7f98297913c8
Heather <heather122333@live.co.uk>
2020-05-19 14:11:38 +0100

    Test correct template

--- blog/tests.py
+++ blog/tests.py
@@ -13,9 +13,11 @@ class HomePageTest(TestCase):
 
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  
-        response = home_page(request)  
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))  
-        self.assertIn('<title>Heather\'s blog</title>', html)  
-        self.assertTrue(html.endswith('</html>'))  
+        response = self.client.get('/')
+
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Heather\'s blog</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
