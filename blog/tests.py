90b4ad014b35cb445e3ae0ec0f6ac4d6939b6544
Heather <heather122333@live.co.uk>
2020-06-08 18:03:10 +0100

    test_redirects_after_POST

--- blog/tests.py
+++ blog/tests.py
@@ -62,10 +62,10 @@ class ItemModelTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    #def test_redirects_after_POST(self):
-    #    response = self.client.post('/', data={'item_text': 'A new list item'})
-    #    self.assertEqual(response.status_code, 302)
-    #    self.assertEqual(response['location'], '/')
+    def test_redirects_after_POST(self):
+        response = self.client.post('/cv/edit/add', data={'item_text': 'A new list item', 'item_category': 'skills'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/cv/edit/add')
 
     #def test_only_saves_items_when_necessary(self):
     #    self.client.get('/')
